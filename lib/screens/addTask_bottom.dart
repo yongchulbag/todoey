b581e14336ea8b1903841a84d0b455a5636b192b
yongchulbag <yongchulbag@gmail.com>
2023-01-08 23:47:44 +0900

    새해다시시작

--- lib/screens/addTask_bottom.dart
+++ lib/screens/addTask_bottom.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 class addTaskBottom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Color(0xff757575),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topRight:Radius.circular(20),
+            topLeft: Radius.circular(20),
+          )
+        ),
+      )
+
+    );
   }
 }
