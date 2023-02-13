d839f762bdbff426b4e2032d3a1c73f86fab15f3
yongchulbag <yongchulbag@gmail.com>
2023-02-14 00:25:00 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/screens/addTask_bottom.dart

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,36 +1,42 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  void passToggleCheckbox(bool newValue) {
+    setState(() {
+      isChecked = newValue;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is A task'),
-      trailing: CheckBoxWidget(),
+      title: Text('This is A task',
+          style: TextStyle(
+              decoration: isChecked ? TextDecoration.lineThrough : null)),
+      trailing: CheckBoxWidget(isChecked, passToggleCheckbox),
     );
   }
 }
 
-class CheckBoxWidget extends StatefulWidget {
-  const CheckBoxWidget({Key? key}) : super(key: key);
+class CheckBoxWidget extends StatelessWidget {
+  final bool isChecked;
+  final Function passedToggleCheckbox;
 
-  @override
-  State<CheckBoxWidget> createState() => _CheckBoxWidgetState();
-}
-
-class _CheckBoxWidgetState extends State<CheckBoxWidget> {
-  bool isChecked = false;
+  CheckBoxWidget(this.isChecked, this.passedToggleCheckbox);
 
   @override
   Widget build(BuildContext context) {
     return Checkbox(
-        activeColor: Colors.lightBlueAccent,
-        value: isChecked,
-        onChanged: (newValue){
-          setState((){
-            isChecked = newValue ?? false;
-          });
-
-        },
-        );
+      activeColor: Colors.lightBlueAccent,
+      value: isChecked,
+      onChanged: passedToggleCheckbox,
+    );
   }
 }
