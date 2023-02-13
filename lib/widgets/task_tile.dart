b7dc8afb36e1008a683686d4d87ee52ff9e06852
yongchulbag <yongchulbag@gmail.com>
2023-02-13 23:01:19 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/screens/addTask_bottom.dart

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -6,7 +6,23 @@ class TaskTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('This is A task'),
-      trailing: Checkbox(value: false, onChanged: null),
+      trailing: CheckBoxWidget(),
     );
   }
 }
+
+class CheckBoxWidget extends StatefulWidget {
+  const CheckBoxWidget({Key? key}) : super(key: key);
+
+  @override
+  State<CheckBoxWidget> createState() => _CheckBoxWidgetState();
+}
+
+class _CheckBoxWidgetState extends State<CheckBoxWidget> {
+  @override
+
+  Widget build(BuildContext context) {
+    return Checkbox(activeColor: Colors.lightBlueAccent, value: false, onChanged: null);
+  }
+}
+
